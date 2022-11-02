55f08723202e0be1b92caee7ad525879a54a0f7d
dee0518 <dehya-518@naver.com>
2022-11-02 17:02:00 +0900

    :lipstick: update  IngredientButtonList styling

--- src/components/IngredientButton.tsx
+++ src/components/IngredientButton.tsx
@@ -8,9 +8,9 @@ interface IngredientButtonProps {
 
 const IngredientButton = styled.button<IngredientButtonProps>`
   border: 0;
-  padding: 4px 12px;
+  padding: 5px 15px;
   border-radius: ${changeRem(15)};
-  font-size: ${changeRem(16)};
+  font-size: ${changeRem(14)};
   color: ${props => (props.color ? props.color : '#7A7A7A')};
   background: ${props => (props.bg ? props.bg : 'rgba(220, 220, 220, 0.3)')};
 `;
