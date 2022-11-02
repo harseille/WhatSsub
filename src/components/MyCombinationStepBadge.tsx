6fac00835a9f215a0f3ce5f08a071acc7d8dcef9
dee0518 <dehya-518@naver.com>
2022-11-02 16:39:16 +0900

    :lipstick: MyCombinationStepBadge updates styling

--- src/components/MyCombinationStepBadge.tsx
+++ src/components/MyCombinationStepBadge.tsx
@@ -3,13 +3,16 @@ import { changeRem } from 'src/styles/mixin';
 
 const MyCombinationStepBadge = styled.span`
   display: inline-block;
-  width: 35px;
-  height: 35px;
+  width: ${changeRem(35)};
+  height: ${changeRem(35)};
   border-radius: 50%;
   border: 3px solid #fbc200;
   background: #098d42;
   color: #fff;
   font-size: ${changeRem(16)};
+  font-weight: bold;
+  line-height: 27px;
+  text-align: center;
 `;
 
 export default MyCombinationStepBadge;
