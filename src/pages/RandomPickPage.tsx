d22b4e7ebdf850428140442d31e0e1613e1f623c
Dada K <dadak@Dadas-MacBook-Air.local>
2022-11-05 11:12:06 +0900

    :lipstick: Fixed Roulette UI and modal stuffs

--- src/pages/RandomPickPage.tsx
+++ src/pages/RandomPickPage.tsx
@@ -1,33 +1,43 @@
 import RandomRoulette from '@components/UI/RandomRoulette';
 import RandomModalResult from '@components/UI/RandomModalResult';
+import DimmedLayer from '@components/UI/DimmedLayer';
 import Wrapper from '@components/UI/Wrapper';
+import styled from '@emotion/styled';
+import { changeRem } from '@styles/mixin';
 
 function RandomPickPage() {
   return (
     <Wrapper>
-      {/* <Title>랜덤 조합 룰렛</Title> */}
+      <Title>랜덤 조합 룰렛</Title>
       <RandomRoulette />
-      {/* <Text>돌려돌려 돌림판~</Text> */}
+      <Text>돌려돌려 돌림판~</Text>
+      <DimmedLayer />
       <RandomModalResult />
     </Wrapper>
   );
 }
 
-// const Title = styled.div`
-//   background-color: #fff;
-//   text-align: center;
-// `;
+const Title = styled.div`
+  text-align: center;
+  margin: 40px 0 20px;
+  font-size: 24px;
+  font-weight: 700;
+`;
 
-// const Text = styled.div`
-//   box-sizing: border-box;
-//   font-size: 20px;
-//   font-weight: 400;
-//   padding: 40px;
-//   color: #fff;
-//   background-color: ${props => props.theme.colors.primaryGreen};
-//   height: ${changeRem(109)};
-//   text-align: center;
-//   margin-bottom: 0;
-// `;
+const Text = styled.div`
+  position: fixed;
+  width: 100%;
+  left: 0;
+  bottom: 77px;
+  box-sizing: border-box;
+  font-size: 20px;
+  font-weight: 400;
+  padding: 30px;
+  color: #fff;
+  background-color: ${props => props.theme.colors.primaryGreen};
+  height: ${changeRem(90)};
+  text-align: center;
+  margin-bottom: 0;
+`;
 
 export default RandomPickPage;
