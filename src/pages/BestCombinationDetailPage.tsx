30e5b44bc0bf95e2ba40466b563e89b3ddaf0f39
harseille <joonhabaak@gmail.com>
2022-11-16 19:53:53 +0900

    :sparkles: loader 적용 꿀조합 디테일 페이지

--- src/pages/BestCombinationDetailPage.tsx
+++ src/pages/BestCombinationDetailPage.tsx
@@ -8,109 +8,24 @@ import SandwitchInfo from '@components/UI/SandwitchInfo';
 import Wrapper from '@components/UI/Wrapper';
 import Like from '@components/UI/Button/Like';
 
-// import ChickenSlice from '@assets/images/Chicken_Slice.png';
-// import sauceHotChilli from '@assets/images/ingredients/sauce/hot_chilli.png';
-
 import { changeRem, flexbox } from '@styles/mixin';
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { LoaderFunctionArgs, useLoaderData } from 'react-router-dom';
 import { API_URL_PATH_PREFIX } from '@constants/constants';
 import { 인터페이스_꿀조합상세페이지_꿀조합 } from '../types/ISandwitch';
-
-// import { 인터페이스_꿀조합 } from '../types/ISandwitch';
-
-// const data: 인터페이스_꿀조합[] = [
-//   {
-//     id: 'k1',
-//     베이스샌드위치: '서브웨이 클럽',
-//     이미지: ChickenSlice,
-//     작성일: '2022.11.01',
-//     작성자: '최원오',
-//     제목: '서브웨이 클럽',
-//     좋아요: '30',
-//     칼로리: '1000000',
-//     선택재료: [
-//       {
-//         id: 'v1',
-//         이름: '양상추',
-//         카테고리: '야채',
-//         칼로리: '30',
-//         재료사진: sauceHotChilli,
-//         추가재료여부: false,
-//       },
-//       {
-//         id: 'v2',
-//         이름: '토마토',
-//         카테고리: '야채',
-//         칼로리: '50',
-//         재료사진: sauceHotChilli,
-//         추가재료여부: false,
-//       },
-//       {
-//         id: 's1',
-//         이름: '핫소스',
-//         카테고리: '소스',
-//         칼로리: '50',
-//         재료사진: sauceHotChilli,
-//         추가재료여부: false,
-//       },
-//       {
-//         id: 's2',
-//         이름: '핫소스',
-//         카테고리: '소스',
-//         칼로리: '50',
-//         재료사진: sauceHotChilli,
-//         추가재료여부: false,
-//       },
-//       {
-//         id: 's3',
-//         이름: '핫소스',
-//         카테고리: '소스',
-//         칼로리: '50',
-//         재료사진: sauceHotChilli,
-//         추가재료여부: false,
-//       },
-//       {
-//         id: 's1',
-//         이름: '핫소스',
-//         카테고리: '소스',
-//         칼로리: '50',
-//         재료사진: sauceHotChilli,
-//         추가재료여부: false,
-//       },
-//     ],
-//     뱃지리스트: {
-//       맛: ['달달', '고소'],
-//       메인재료: ['살라미'],
-//       추가사항: ['고기러버'],
-//     },
-//   },
-// ];
-
-// ? fetched를 뭐라고 바꿔야될까
-// const 꿀조합_데이터 = {
-//   작성자:
-// };
 
 // ? fetch를 뭐라고 바꿀지 고민...
 const fetchJson = (url: string) => fetch(url).then(res => res.json());
 const getResponseByBestCombinationId = (combinationId = '') => fetchJson(`${API_URL_PATH_PREFIX}/bestCombination.json`);
 
+const 꿀조합_데이터_받아오기 = async (
+  combinationId: string | undefined
+): Promise<인터페이스_꿀조합상세페이지_꿀조합> => {
+  const response = await getResponseByBestCombinationId(combinationId);
+  return response;
+};
+
 function BestCombinationDetailPage() {
-  const [꿀조합, 꿀조합_설정] = useState<인터페이스_꿀조합상세페이지_꿀조합 | null>(null);
-  const { combinationId } = useParams();
-
-  const 꿀조합_데이터_받아오기 = async (combinationId: string | undefined) => {
-    const response = await getResponseByBestCombinationId(combinationId);
-
-    꿀조합_설정(response);
-  };
-
-  useEffect(() => {
-    꿀조합_데이터_받아오기(combinationId);
-  }, []);
-
-  // const { displayName } = getSessionUserInfo();
+  const 꿀조합: 인터페이스_꿀조합상세페이지_꿀조합 | any = useLoaderData();
 
   if (꿀조합) {
     return (
@@ -119,17 +34,11 @@ function BestCombinationDetailPage() {
           <h1>
             <span>{꿀조합.작성자_이름}</span>만의 조합
           </h1>
-          {/* <Like count={data[0].좋아요} /> */}
           <Like count={꿀조합.좋아요} />
         </Header>
         <Contents>
           <SandwitchInfo
             sandwitch={{
-              // 이미지: data[0].이미지,
-              // 이름: data[0].제목,
-              // 베이스샌드위치: data[0].베이스샌드위치,
-              // 칼로리: data[0].칼로리,
-              // 뱃지리스트: data[0].뱃지리스트,
               이미지: 꿀조합.이미지,
               이름: 꿀조합.제목,
               베이스샌드위치: 꿀조합.베이스샌드위치,
@@ -137,11 +46,8 @@ function BestCombinationDetailPage() {
               뱃지리스트: 꿀조합.뱃지리스트,
             }}
           />
-          {/* <IngredientCardList ingredientList={data[0].선택재료} /> */}
-          {/* <CombinationIngredientList ingredientList={data[0].선택재료} /> */}
           <IngredientCardList ingredientList={꿀조합.선택재료} />
           {/* <CombinationIngredientList ingredientList={꿀조합.선택재료} /> */}
-          {/* <CombinationIngredientList /> */}
         </Contents>
         <Comments>
           <CommentHeader>
@@ -155,16 +61,13 @@ function BestCombinationDetailPage() {
       </Wrapper>
     );
   }
-
-  return <div>fallback</div>;
+  // TODO: 꿀조합 찾기 Error Fallback page 개발
+  return <div>Fallback</div>;
 }
 
 export default BestCombinationDetailPage;
 
-// export function loader({ params }) {
-//   const { combinationId } = params;
-//   return getPost(combinationId);
-// }
+export const loader = ({ params }: LoaderFunctionArgs) => 꿀조합_데이터_받아오기(params.combinationId);
 
 const Header = styled.div`
   ${flexbox('row', 'space-between', 'center')}
@@ -212,7 +115,3 @@ const CommentHeader = styled.div`
     }
   }
 `;
-
-// const loader = () => {
-//   // test
-// };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import LoginPage from './LoginPage';
 import MyPage from './MyPagePage';
 import BestCombinationPickPage from './BestCombinationPickPage';
 import BestCombinationListPage from './BestCombinationListPage';
-import BestCombinationDetailPage from './BestCombinationDetailPage';
+import BestCombinationDetailPage, { loader as bestCombinationDetailLoader } from './BestCombinationDetailPage';
 import RandomPickPage from './RandomPickPage';
 import RankingPage from './RankingPage';
 import CustomCombination from './CustomCombinationPage';
@@ -18,4 +18,5 @@ export {
   RandomPickPage,
   RankingPage,
   CustomCombination,
+  bestCombinationDetailLoader,
 };
