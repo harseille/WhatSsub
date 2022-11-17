b385e34ba450d41f3a355fb910367ee9348f14bf
Dada K <dadak@Dadas-MacBook-Air.local>
2022-11-17 18:11:48 +0900

    :lipstick: add spin animation

--- src/components/UI/RandomRoulette.tsx
+++ src/components/UI/RandomRoulette.tsx
@@ -1,14 +1,34 @@
+import { useRef } from 'react';
 import styled from '@emotion/styled';
-import spinBoard from '@assets/images/spinBoard.png';
+import spinBoard from '@assets/images/roulette.png';
 import startBtn from '@assets/images/startBtn.png';
 import pointer from '@assets/images/pointer.png';
 import { changeRem } from '@styles/mixin';
 
 function RandomRoulette() {
+  const roulette = useRef<string | any>();
+
+  const 룰렛_회전 = () => {
+    const random = Math.floor(+Math.random * 6);
+    const rotate = 3000;
+    roulette.current.style.transform = `rotate(-${rotate}deg)`;
+    roulette.current.style.transition = `2s`;
+    return random;
+  };
+
+  const 룰렛_돌리기 = () => {
+    룰렛_회전();
+    console.log('회전 클릭 이벤트');
+    setTimeout(() => {
+      console.log('3초');
+      // 여기에 모달 뜨게
+    }, 3000);
+  };
+
   return (
     <Container>
-      <Roulette src={spinBoard} alt="룰렛" />
-      <StartButton src={startBtn} alt="시작 버튼" />
+      <Roulette src={spinBoard} alt="룰렛" ref={roulette} />
+      <StartButton src={startBtn} alt="시작 버튼" onClick={룰렛_돌리기} />
       <Pointer src={pointer} alt="포인터" />
     </Container>
   );
@@ -21,11 +41,7 @@ const Container = styled.div`
   padding-top: 18px;
 `;
 const Roulette = styled.img`
-  /* 
-  left: 50%;
-  top: 18%;
-  transform: translate(-50%, -4%);
-  width: ${changeRem(316)}; */
+  /* transform: rotate(-${200}deg); */
 `;
 const Pointer = styled.img`
   position: absolute;
