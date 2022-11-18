09d7b82769c1ef3c2813475839d31c7719a894c6
choi1five <cwo1401@gmail.com>
2022-11-18 23:27:30 +0900

    :sparkles: add custom_hook for Scroll_Top

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,13 +2,18 @@ import { Outlet } from 'react-router-dom';
 import Header from '@layouts/Header';
 import styled from '@emotion/styled';
 import mediaQuery from '@styles/media-queries';
+import Top from '@components/Common/Button/Top';
+import useScrollTop from '../hooks/useScrollTop';
 
 function RootLayout() {
+  const { isShowTop } = useScrollTop();
+
   return (
     <Layout>
       <Header />
       <Main>
         <Outlet />
+        {isShowTop && <Top />}
       </Main>
     </Layout>
   );
