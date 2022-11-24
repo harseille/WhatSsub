12f29d2affc76e027bba8a63931031e2e63f5e3d
Dada K <dadak@Dadas-MacBook-Air.local>
2022-11-24 17:17:26 +0900

    :memo: test

--- src/pages/MyPagePage.tsx
+++ src/pages/MyPagePage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
-import { isLoggedInState } from '@state/index';
+import { isLoggedInState, userState } from '@state/index';
 import SandwichInfo from '@components/Sandwich/SandwichInfo';
 import Wrapper from '@components/UI/Wrapper';
 import styled from '@emotion/styled';
@@ -12,6 +12,8 @@ import { changeRem } from '@styles/mixin';
 import { 인터페이스_꿀조합 } from '@typings/ISandwich';
 import dbGet from '@api/dbGet';
 import { collection, orderBy, query } from 'firebase/firestore';
+import { User } from 'firebase/auth';
+import { AnyTxtRecord } from 'dns';
 import { db } from '../firebase.config';
 
 export interface 인터페이스_꿀조합_아이디 extends 인터페이스_꿀조합 {
@@ -72,6 +74,8 @@ function MyPage() {
   const navigate = useNavigate();
   const isLoggedin = useRecoilValue(isLoggedInState);
   const [currentTab, setCurrentTab] = useState<string>('좋아요 꿀조합');
+  const [myList, setMyList] = useState<인터페이스_꿀조합_아이디[] | null>(null);
+  const 유저정보: User | null = useRecoilValue(userState);
 
   // db.collection('꿀조합') =>
 
@@ -86,25 +90,33 @@ function MyPage() {
 
   const [toggleState, setToggleState] = useState<boolean | undefined>(true);
 
+  // const 나만의_리스트: 인터페이스_꿀조합_아이디[] = [];
+  // ? --------------------------------------------------------------------------------------------------------------------
   const 클릭핸들러_꿀조합_목록_변경 = (e: React.MouseEvent<HTMLElement>) => {
-    const 사용자명_체크 = (e.target as HTMLSpanElement).textContent?.includes('단찌');
+    const 사용자명_체크 = (e.target as HTMLSpanElement).textContent?.includes(`${유저정보?.displayName}`);
     setToggleState(사용자명_체크);
   };
   const 꿀조합_컬렉션_탭에따라_가져오기 = async (tabToggle: string) => {
     const 쿼리스냅샷 = await dbGet(query(collection(db, '꿀조합'), orderBy(tabToggle, 'desc'))); // tabToggle에 따라 내림차순
-    console.log('쿼리 스냅샷 =>', 쿼리스냅샷);
 
-    const 랭킹리스트: 인터페이스_꿀조합_아이디[] = [];
-
+    const 나만의_리스트: 인터페이스_꿀조합_아이디[] = [];
     await 쿼리스냅샷.forEach(doc => {
-      랭킹리스트.push({ id: doc.id, ...JSON.parse(JSON.stringify(doc.data())) });
+      나만의_리스트.push({ id: doc.id, ...JSON.parse(JSON.stringify(doc.data())) });
     });
-    console.log('랭킹리스트 =>', 랭킹리스트);
-  };
+    // console.log('나만의 리스트 =>', 나만의_리스트);
+    setMyList(나만의_리스트);
 
-  // const 꿀조합_정렬해서_가져오기 = async() => {
-  //   const 쿼리스냅샷 = await dbGet(query(collection(db, '꿀조합'), orderBy(데이터에 담긴애랑 여기서 걸른 로직의 유저 변수값이 들어가야함, 'desc')));
-  // }
+    // const 유저만의_꿀조합 = 나만의_리스트.filter((user: 인터페이스_꿀조합_아이디) => user.작성자id === 유저정보?.uid);
+    // console.log('유저만의_꿀조합 =>', 유저만의_꿀조합);
+  };
+  // ? --------------------------------------------------------------------------------------------------------------------
+
+  // console.log('나만의_리스트 =>', 나만의_리스트);
+  // console.log('sandwiches =>', sandwiches);
+  // console.log('myList =>', myList);
+
+  const 유저만의_꿀조합 = myList?.filter((user: 인터페이스_꿀조합_아이디) => user.작성자id === 유저정보?.uid);
+  console.log('유저만의_꿀조합 =>', 유저만의_꿀조합);
 
   //! 서버에서 sort 해주면 얘도 없어질 예정
   const 날짜_내림차순_꿀조합_목록 = (prev: 인터페이스_꿀조합, next: 인터페이스_꿀조합): number => {
@@ -116,7 +128,8 @@ function MyPage() {
     +next.좋아요 - +prev.좋아요;
 
   // ! 데이터 받아온 후 컴포넌트를 만들게 됨으로 리팩토링 예정
-  const userCombination = sandwiches.sort(날짜_내림차순_꿀조합_목록).map(sandwich => (
+  // const userCombination = sandwiches.sort(날짜_내림차순_꿀조합_목록).map(sandwich => (
+  const userCombination = 유저만의_꿀조합?.map(sandwich => (
     <Card key={sandwich.작성자id}>
       <Link to={`/best-combination/${sandwich.꿀조합제목}`}>
         <SandwichInfo sandwich={sandwich} />
@@ -137,7 +150,9 @@ function MyPage() {
       <Content>
         {toggleState ? (
           <div>
-            <UserTitle onClick={클릭핸들러_꿀조합_목록_변경}>단찌만의 조합</UserTitle>
+            <UserTitle onClick={클릭핸들러_꿀조합_목록_변경}>
+              <span>{유저정보?.displayName}</span>만의 조합
+            </UserTitle>
             <LikeTitle className="sub-title" onClick={클릭핸들러_꿀조합_목록_변경}>
               좋아요 꿀조합
             </LikeTitle>
@@ -145,7 +160,7 @@ function MyPage() {
         ) : (
           <div>
             <UserTitle onClick={클릭핸들러_꿀조합_목록_변경} style={{ color: '#6b6b6b' }}>
-              단찌만의 조합
+              <span>{유저정보?.displayName}</span>만의 조합
             </UserTitle>
             <LikeTitle style={{ color: '#252525' }} className="sub-title" onClick={클릭핸들러_꿀조합_목록_변경}>
               좋아요 꿀조합
