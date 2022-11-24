183bb5c199dd8aff01650b87465594ef7805f4fb
harseille <joonhabaak@gmail.com>
2022-11-24 09:33:59 +0900

    :sparkles: Comments ordering

--- src/components/Comments/CommentList.tsx
+++ src/components/Comments/CommentList.tsx
@@ -2,6 +2,8 @@ import CommentItem from '@components/Comments/CommentItem';
 import { 인터페이스_댓글_읽기 } from '@typings/IComment';
 
 function CommentList({ commentList }: { commentList: 인터페이스_댓글_읽기[] }) {
+  console.log(commentList);
+
   const 댓글목록 = commentList.map(댓글 => (
     <li key={댓글.댓글id}>
       <CommentItem comment={댓글} />
--- src/components/Comments/CommentsContainer.tsx
+++ src/components/Comments/CommentsContainer.tsx
@@ -6,17 +6,18 @@ import CommentInputWrap from '@components/Comments/CommentInputWrap';
 import styled from '@emotion/styled';
 import { changeRem, flexbox } from '@styles/mixin';
 import useGetComments from '@hooks/useGetComments';
+import mediaQuery from '@styles/media-queries';
 
 function CommentsContainer() {
   const { combinationId } = useParams();
-  const { comments } = useGetComments(combinationId);
+  const { comments, commentsCount } = useGetComments(combinationId);
   const isLoggedIn = useRecoilValue(isLoggedInState);
 
   return (
     <Comments>
       <CommentHeader>
         <h2>
-          리뷰 <span>{comments.length}</span>
+          리뷰 <span>{commentsCount}</span>
         </h2>
       </CommentHeader>
       <CommentList commentList={comments} />
@@ -30,8 +31,12 @@ export default CommentsContainer;
 const Comments = styled.div`
   background: #fff;
   padding: 0 32px;
-  padding-bottom: 64px;
+  padding-bottom: ${changeRem(60)};
   box-shadow: 0px -4px 10px rgba(213, 213, 213, 0.25);
+
+  ${mediaQuery} {
+    padding-bottom: ${changeRem(80)};
+  }
 `;
 
 const CommentHeader = styled.div`
