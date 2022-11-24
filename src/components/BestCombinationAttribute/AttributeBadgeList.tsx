587c1051a45ac131e160158c658e07828e77d988
Dada K <dadak@Dadas-MacBook-Air.local>
2022-11-24 18:05:52 +0900

    :memo: connected mypage firebase except for like tab list

--- src/components/BestCombinationAttribute/AttributeBadgeList.tsx
+++ src/components/BestCombinationAttribute/AttributeBadgeList.tsx
@@ -1,28 +1,29 @@
 import IngredientBadge from '@components/BestCombinationAttribute/AttributeBadge';
 import styled from '@emotion/styled';
 import mediaQuery from '@styles/media-queries';
-import { 인터페이스_샌드위치뱃지리스트 } from '@typings/ISandwich';
+// import { 인터페이스_샌드위치뱃지리스트 } from '@typings/ISandwich';
+import convertBadge from '@utils/convertBadge';
 
-function SandwichBadgeList({ badgeList }: { badgeList: 인터페이스_샌드위치뱃지리스트 }) {
-const convertedBadgeList = Object.entries(badgeList);
+function SandwichBadgeList({ badgeList }: { badgeList: string[] }) {
+  const convertedBadgeList = convertBadge(badgeList);
 
-return (
-<BadgeList>
-{convertedBadgeList.map(([filter, badgeList]) =>
-badgeList.map((badge: string) => <IngredientBadge key={badge} filter={filter} item={badge} />)
-)}
-</BadgeList>
-);
+  return (
+    <BadgeList>
+      {convertedBadgeList.map(([filter, badgeList]) =>
+        badgeList.map((badge: string) => <IngredientBadge key={badge} filter={filter} item={badge} />)
+      )}
+    </BadgeList>
+  );
 }
 
 const BadgeList = styled.ul`
-display: flex;
-flex-flow: wrap;
-margin: 0;
-gap: 8px;
-${mediaQuery} {
-width: 300px;
-}
+  display: flex;
+  flex-flow: wrap;
+  margin: 0;
+  gap: 8px;
+  ${mediaQuery} {
+    width: 300px;
+  }
 `;
 
 export default SandwichBadgeList;
