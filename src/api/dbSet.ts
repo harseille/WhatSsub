8393d5d323a9b2947354875bf77804dfa2713b94
Dada K <dadak@Dadas-MacBook-Air.local>
2022-11-25 16:39:08 +0900

    :memo: 누락된 수정사항

--- src/api/dbSet.ts
+++ src/api/dbSet.ts
@@ -1,9 +1,9 @@
-import { updateDoc, collection, doc } from 'firebase/firestore';
+import { setDoc, collection, doc } from 'firebase/firestore';
 import { db } from '../firebase.config';
 
 const dbSet = async (콜랙션: string, 문서: string, 데이터: object) => {
   try {
-    return await updateDoc(doc(collection(db, 콜랙션), 문서), 데이터);
+    return await setDoc(doc(collection(db, 콜랙션), 문서), 데이터);
   } catch {
     console.log('db set 실패');
   }
