d0ce92991b59400e80569095cfcbeff3ebc49a2d
choi1five <cwo1401@gmail.com>
2022-11-26 22:40:12 +0900

    :recycle: add Attribute as QueryString

--- src/components/BestCombinationList/BestCombinationList.tsx
+++ src/components/BestCombinationList/BestCombinationList.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect, useCallback } from 'react';
 import useCheckLogin from '@hooks/useCheckLogin';
-import { collection, orderBy, query, where } from 'firebase/firestore';
+import { collection, orderBy, query } from 'firebase/firestore';
 import NotFoundBestCombinationList from '@components/BestCombinationList/NotFoundBestCombinationList';
 import SandwichInfoCard from '@components/Common/Cards/SandwichInfoCard';
 import Modal from '@components/UI/Modal';
@@ -8,20 +9,22 @@ import dbGet from '@api/dbGet';
 import styled from '@emotion/styled';
 import { flexbox } from '@styles/mixin';
 import { 인터페이스_꿀조합 } from '@typings/ISandwich';
-import useInfiniteScroll from '@hooks/useInfiniteScroll';
+import 꿀조합_목록_필터링하기 from '@utils/filterBestCombinationList';
+// import useInfiniteScroll from '@hooks/useInfiniteScroll';
 
 import { db } from '../../firebase.config';
 
 function BestCombinationList({ filter }: { filter: string[] }) {
-  const [꿀조합_목록, 꿀조합_목록_수정] = useState<인터페이스_꿀조합[]>([]);
+  const [꿀조합_목록, 꿀조합_목록_수정] = useState<인터페이스_꿀조합[] | null>(null);
   const { userInfo, isShowModal, toggleModal, navigateLoginPage } = useCheckLogin();
+  const { state } = useLocation();
 
-  const fetchData = async () => {
+  const 꿀조합_불러오기 = useCallback(async () => {
     const 꿀조합_컬렉션 = collection(db, '꿀조합');
-    const 꿀조합쿼리 =
-      filter.length === 0
-        ? query(꿀조합_컬렉션, orderBy('좋아요', 'desc'))
-        : query(꿀조합_컬렉션, where('뱃지리스트', 'array-contains', filter));
+    const 꿀조합쿼리 = query(꿀조합_컬렉션, orderBy('좋아요', 'desc'));
+    // filter.length === 0
+    //   ? query(꿀조합_컬렉션, orderBy('좋아요', 'desc'))
+    //   : query(꿀조합_컬렉션, where('뱃지리스트', 'array-contains', filter));
 
     const 쿼리스냅샷 = await dbGet(꿀조합쿼리);
 
@@ -33,20 +36,25 @@ function BestCombinationList({ filter }: { filter: string[] }) {
       });
     });
 
-    꿀조합_목록_수정(convertBCList);
-  };
+    const 필터링된_꿀조합 = 꿀조합_목록_필터링하기(convertBCList, state);
 
-  // const { listRef, isLoading } = useInfiniteScroll(fetchData, 10, '꿀조합');
-  const { listRef, isLoading } = useInfiniteScroll(fetchData, 10, query(collection(db, '꿀조합')));
+    꿀조합_목록_수정(필터링된_꿀조합);
+  }, [state]);
 
-  if (isLoading)
-    return (
-      <ul>
-        <li ref={listRef}>Loading.....</li>
-      </ul>
-    );
+  useEffect(() => {
+    꿀조합_불러오기();
+  }, [꿀조합_불러오기]);
 
-  if (꿀조합_목록.length === 0) return <NotFoundBestCombinationList />;
+  // const { listRef, isLoading } = useInfiniteScroll(fetchData, 10, query(collection(db, '꿀조합')));
+
+  // if (isLoading)
+  //   return (
+  //     <ul>
+  //       <li ref={listRef}>Loading.....</li>
+  //     </ul>
+  //   );
+
+  if (꿀조합_목록?.length === 0) return <NotFoundBestCombinationList />;
 
   return (
     <>
@@ -60,7 +68,7 @@ function BestCombinationList({ filter }: { filter: string[] }) {
         />
       )}
       <ListWrap>
-        {꿀조합_목록.map(sandwich => (
+        {꿀조합_목록?.map(sandwich => (
           //* key 값 수정 필요
           <SandwichInfoCard key={sandwich.id} sandwich={sandwich} userInfo={userInfo} toggleModal={toggleModal} />
         ))}
