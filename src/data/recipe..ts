855b7f48f2a637ff079fcc70cd2c378dfc604c13
jeongsehun <tpgns933@gmail.com>
2022-11-27 23:47:44 +0900

    :pencil2: 클라이언트 에 있던 json 데이터 파일 객체로 변경

---
+++ src/data/recipe..ts
@@ -0,0 +1,112 @@
+const recipe = [
+  {
+    이름: '스파이시 바비큐',
+    재료목록: ['스파이시 바비큐'],
+    재료칼로리: '165.7',
+    이미지: '/images/sandwich/SpicyBBQ.png',
+  },
+  {
+    이름: 'K-바비큐',
+    재료목록: ['돼지고기'],
+    재료칼로리: '163.7',
+    이미지: '/images/sandwich/K-BBQ.png',
+  },
+  {
+    이름: '풀드 포크 바비큐',
+    재료목록: ['돼지고기'],
+    재료칼로리: '163.7',
+    이미지: '/images/sandwich/PulledPorkBarbecue.png',
+  },
+  {
+    이름: '이탈리안 비엠티',
+    재료목록: ['페퍼로니', '살라미', '햄'],
+    재료칼로리: '179.5',
+    이미지: '/images/sandwich/ItalianB.M.T.png',
+  },
+  {
+    이름: '써브웨이 클럽',
+    재료목록: ['닭가슴살 햄', '베이컨'],
+    재료칼로리: '90.4',
+    이미지: '/images/sandwich/SubwayClub.png',
+  },
+  {
+    이름: '스테이크 & 치즈',
+    재료목록: ['스테이크'],
+    재료칼로리: '146.7',
+    이미지: '/images/sandwich/Steak&Cheese.png',
+  },
+  {
+    이름: '치킨 베이컨 아보카도',
+    재료목록: ['닭가슴살 햄', '베이컨', '아보카도'],
+    재료칼로리: '146.4',
+    이미지: '/images/sandwich/ChickenBaconAvocado.png',
+  },
+  {
+    이름: '로티세리 바비큐 치킨',
+    재료목록: ['로티세리 치킨'],
+    재료칼로리: '118.8',
+    이미지: '/images/sandwich/RotisserieBarbecueChicken.png',
+  },
+  {
+    이름: '로스트 치킨',
+    재료목록: ['닭가슴살'],
+    재료칼로리: '91.7',
+    이미지: '/images/sandwich/RoastedChicken.png',
+  },
+  {
+    이름: '쉬림프',
+    재료목록: ['새우'],
+    재료칼로리: '20.7',
+    이미지: '/images/sandwich/Shrimp.png',
+  },
+  {
+    이름: '치킨 데리야끼',
+    재료목록: ['치킨 데리야끼'],
+    재료칼로리: '105.7',
+    이미지: '/images/sandwich/ChickenTeriyaki.png',
+  },
+  {
+    이름: '스파이시 이탈리안',
+    재료목록: ['페퍼로니', '살라미'],
+    재료칼로리: '254.5',
+    이미지: '/images/sandwich/SpicyItalian.png',
+  },
+  {
+    이름: '비엘티',
+    재료목록: ['베이컨'],
+    재료칼로리: '91.6',
+    이미지: '/images/sandwich/B.L.T.png',
+  },
+  {
+    이름: '치킨 슬라이스',
+    재료목록: ['닭가슴살 햄'],
+    재료칼로리: '56.8',
+    이미지: '/images/sandwich/ChickenSlice.png',
+  },
+  {
+    이름: '햄',
+    재료목록: ['햄'],
+    재료칼로리: '53.6',
+    이미지: '/images/sandwich/Ham.png',
+  },
+  {
+    이름: '참치',
+    재료목록: ['참치'],
+    재료칼로리: '107.6',
+    이미지: '/images/sandwich/Tuna.png',
+  },
+  {
+    이름: '에그마요',
+    재료목록: ['에그마요'],
+    재료칼로리: '207.7',
+    이미지: '/images/sandwich/EggMayo.png',
+  },
+  {
+    이름: '베지',
+    재료목록: ['모둠야채'],
+    재료칼로리: '17',
+    이미지: '/images/sandwich/VeggieDelite.png',
+  },
+];
+
+export default recipe;
