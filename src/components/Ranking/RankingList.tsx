3c32e48be0b2ae84654f20f10c77f83544949964
dee0518 <dehya-518@naver.com>
2022-11-28 16:46:30 +0900

    :lipstick: 신규 뱃지 추가

--- src/components/Ranking/RankingList.tsx
+++ src/components/Ranking/RankingList.tsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 import { collection, DocumentData, query } from 'firebase/firestore';
 import useInfiniteScroll from '@hooks/useInfiniteScroll';
 import CombinationRankingCard from '@components/Ranking/CombinationRankingCard';
+import getYearMonthDate from '@utils/getYearMonthDate';
 import getRankingList from '@api/getRankingList';
 import Rank1 from '@assets/images/rankingBadge/rank_1.png';
 import Rank2 from '@assets/images/rankingBadge/rank_2.png';
 import Rank3 from '@assets/images/rankingBadge/rank_3.png';
 import styled from '@emotion/styled';
 import mediaQuery from '@styles/media-queries';
+import { changeRem } from '@styles/mixin';
 import { 인터페이스_꿀조합 } from '@typings/ISandwich';
 import { db } from '../../firebase.config';
 
@@ -50,22 +52,29 @@ function RankingList({ currentTab }: TProps) {
 
   return (
     <ul>
-      {rankingList !== null &&
-        rankingList.map(({ id, 꿀조합제목, 이미지, 베이스샌드위치, 뱃지리스트, 좋아요 }, i) => {
-          let badgeUrl = '';
+      {rankingList.length > 0 &&
+        rankingList.map(({ id, 꿀조합제목, 이미지, 베이스샌드위치, 작성일, 뱃지리스트, 좋아요 }, i) => {
+          let 랭킹_뱃지_이미지 = '';
+          let 신규_샌드위치인가 = false;
 
           if (currentTab === '맛잘알랭킹') {
             const { FIRST, SECOND, THIRD } = Ranking;
 
-            if (i === FIRST.id) badgeUrl = FIRST.imageSrc;
-            else if (i === SECOND.id) badgeUrl = SECOND.imageSrc;
-            else if (i === THIRD.id) badgeUrl = THIRD.imageSrc;
+            if (i === FIRST.id) 랭킹_뱃지_이미지 = FIRST.imageSrc;
+            else if (i === SECOND.id) 랭킹_뱃지_이미지 = SECOND.imageSrc;
+            else if (i === THIRD.id) 랭킹_뱃지_이미지 = THIRD.imageSrc;
+          } else if (currentTab === '신규조합') {
+            const { year: 오늘_년도, month: 오늘_월, date: 오늘_일 } = getYearMonthDate(new Date());
+            const { year: 작성일_년도, month: 작성일_월, date: 작성일_일 } = getYearMonthDate(new Date(작성일));
+
+            if (오늘_년도 === 작성일_년도 && 오늘_월 === 작성일_월 && 오늘_일 === 작성일_일) 신규_샌드위치인가 = true;
           }
 
           return (
             <li key={id} ref={i === rankingList.length - 1 ? listRef : null}>
               <RankingCardWrapper to={`/best-combination/${id}`}>
-                {badgeUrl && <RankBadge src={badgeUrl} alt={`rank${i + 1}`} />}
+                {신규_샌드위치인가 && <NewBadge>NEW</NewBadge>}
+                {랭킹_뱃지_이미지 && <RankBadge src={랭킹_뱃지_이미지} alt={`rank${i + 1}`} />}
                 <CombinationRankingCard
                   rank={i + 1}
                   currentTab={currentTab}
@@ -102,5 +111,22 @@ const RankBadge = styled.img`
     width: 106px;
     top: -40px;
     right: -25px;
+  }
+`;
+
+const NewBadge = styled.span`
+  position: absolute;
+  top: -6px;
+  right: 20px;
+  z-index: 1;
+  padding: 3px 5px;
+  border-radius: 3px;
+  background-color: ${props => props.theme.colors.primaryYellow};
+  font-size: ${changeRem(10)};
+  font-weight: bold;
+  color: #fff;
+
+  ${mediaQuery} {
+    top: -8px;
   }
 `;
