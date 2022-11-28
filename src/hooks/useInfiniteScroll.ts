478154690d4714422abd82ae6e2e5d8a6f8c64d7
dee0518 <dehya-518@naver.com>
2022-11-28 13:14:18 +0900

    :bug: 랭킹리스트 탭 변경시 firebase요청 및 무한 스크롤관련 오류 수정

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -1,16 +1,17 @@
 import { useState, useCallback, useRef, useEffect } from 'react';
 import { query, getCountFromServer, Query, DocumentData } from 'firebase/firestore';
 
-// const useInfiniteScroll = (callback: Function, dataLength: number, collectionName: string) => {
 const useInfiniteScroll = (callback: Function, dataLength: number, dbCountQuery: Query<DocumentData>) => {
   const [hasMore, setHasMore] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getServerDataLength = async () => {
-      // const querySnapshot = await getCountFromServer(query(collection(db, collectionName)));
       const querySnapshot = await getCountFromServer(query(dbCountQuery));
 
+      if (dataLength === 0) {
+        setHasMore(true);
+      }
       if (dataLength >= querySnapshot.data().count) {
         setHasMore(false);
       }
@@ -21,7 +22,6 @@ const useInfiniteScroll = (callback: Function, dataLength: number, dbCountQuery:
   const observer = useRef<IntersectionObserver | null>(null);
   const listRef = useCallback(
     (node: HTMLLIElement) => {
-      // if (!observer.current) return;
       if (observer.current) observer.current.disconnect();
       observer.current = new IntersectionObserver(
         async entries => {
