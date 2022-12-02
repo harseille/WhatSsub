9beca2b9e46e27e62cc5c67cb0844ac2415b38c6
choi1five <cwo1401@gmail.com>
2022-12-02 22:01:58 +0900

    :recycle: add LoadingSpinner when 꿀조합fetching

--- src/components/BestCombinationList/BestCombinationList.tsx
+++ src/components/BestCombinationList/BestCombinationList.tsx
@@ -6,23 +6,35 @@ import SandwichInfoCard from '@components/Common/Cards/SandwichInfoCard';
 import styled from '@emotion/styled';
 import { flexbox } from '@styles/mixin';
 import { 인터페이스_꿀조합 } from '@typings/ISandwich';
+import LoadingSpinner from '@components/Common/LoadingSpinner';
 
 function BestCombinationList() {
   const [꿀조합_목록, 꿀조합_목록_수정] = useState<인터페이스_꿀조합[] | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const { state } = useLocation();
 
   useEffect(() => {
-    getFilteredBestCombination(꿀조합_목록_수정, state);
+    (async () => {
+      await getFilteredBestCombination(꿀조합_목록_수정, state);
+      setIsLoading(false);
+    })();
   }, [state]);
 
   if (꿀조합_목록?.length === 0) return <NotFoundBestCombinationList />;
 
   return (
-    <ListWrap>
-      {꿀조합_목록?.map(sandwich => (
-        <SandwichInfoCard key={sandwich.id} sandwich={sandwich} />
-      ))}
-    </ListWrap>
+    <>
+      {isLoading && (
+        <LoadingWrap>
+          <LoadingSpinner />
+        </LoadingWrap>
+      )}
+      <ListWrap>
+        {꿀조합_목록?.map(sandwich => (
+          <SandwichInfoCard key={sandwich.id} sandwich={sandwich} />
+        ))}
+      </ListWrap>
+    </>
   );
 }
 
@@ -33,4 +45,10 @@ const ListWrap = styled.ul`
   display: flex;
   ${flexbox('column', 'center', 'center')}
   gap: 16px;
+`;
+
+const LoadingWrap = styled.div`
+  position: fixed;
+  top: 50%;
+  transform: translate(0, -50%);
 `;
