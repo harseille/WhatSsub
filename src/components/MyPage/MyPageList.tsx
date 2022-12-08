4e089dcc7a5e9ff9d6a35f2b00099486f697c636
Dada K <dadak@Dadas-MacBook-Air.local>
2022-12-08 23:37:26 +0900

    :bug: fix filtering

--- src/components/MyPage/MyPageList.tsx
+++ src/components/MyPage/MyPageList.tsx
@@ -1,9 +1,11 @@
 import { memo } from 'react';
 import { useRecoilValue } from 'recoil';
 import { userLike } from '@state/User';
+import { userState } from '@state/index';
 import UserCombinatonList from '@components/MyPage/UserCombinatonList';
 import LikeCombinationList from '@components/MyPage/LikeCombinationList';
 import styled from '@emotion/styled';
+import { User } from 'firebase/auth';
 import { 인터페이스_꿀조합 } from '@typings/ISandwich';
 
 type TProps = {
@@ -13,9 +15,11 @@ type TProps = {
 };
 
 function MyPageList({ isSelectedTab, userCombinationList, onClick }: TProps) {
+  const 유저정보: User | null = useRecoilValue(userState);
   const 좋아요한샌드위치: string[] = useRecoilValue(userLike);
+  console.log('userCombinationList', userCombinationList);
   const 유저만의조합 = isSelectedTab
-    ? userCombinationList
+    ? userCombinationList.filter((꿀조합: 인터페이스_꿀조합) => 꿀조합.작성자id === 유저정보?.uid)
     : userCombinationList.filter((꿀조합: 인터페이스_꿀조합) => 좋아요한샌드위치.includes(꿀조합.id));
 
   return (
