f6b61347b2557c03ecff1d4e241275190aca29e2
Dada K <dadak@Dadas-MacBook-Air.local>
2023-02-03 15:09:49 +0900

    :memo: fix like button error

--- src/components/MyPage/LikeCard.tsx
+++ src/components/MyPage/LikeCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue, useRecoilState } from 'recoil';
 import { userState } from '@state/index';
@@ -25,9 +25,11 @@ function LikeCard({ id, imgUrl, sandwich: sandwichData }: TProps) {
   const 좋아요_버튼_수정하기 = (e: React.MouseEvent<HTMLButtonElement>) => {
     const target = (e.target as HTMLButtonElement).closest('li');
 
+    // 좋아요버튼 눌려있을 때 => id 비교해서 데이터베이스에서 삭제, 삭제 전역에도 추가
     if (좋아요한샌드위치.includes(target!.id) && !삭제될_꿀조합.includes(target!.id)) {
       dbUpdate('좋아요', 유저정보!.uid, { 좋아요_리스트: 좋아요한샌드위치.filter(id => id !== target!.id) });
       삭제될_꿀조합_수정(prev => [...prev, target!.id]);
+      // 좋아요 버튼 해제 됐을 때(펜딩 상태)
     } else {
       dbUpdate('좋아요', 유저정보!.uid, { 좋아요_리스트: [...좋아요한샌드위치] });
       삭제될_꿀조합_수정(prev => prev.filter(삭제될_꿀조합 => 삭제될_꿀조합 !== target!.id));
@@ -43,6 +45,16 @@ function LikeCard({ id, imgUrl, sandwich: sandwichData }: TProps) {
     좋아요한샌드위치_수정(prevData => prevData.filter(prev => !삭제될_꿀조합.includes(prev)));
     삭제될_꿀조합_수정([]);
   };
+
+  // const 타이머_셋하기 = () => {
+  //   타이머_수정(setInterval(맛잘알_리스트_가져오기.bind(null, 랭킹리스트.length), 60000));
+  // };
+
+  // const 타이머_멈추기 = () => {
+  //   clearInterval(타이머);
+  // };
+
+  useEffect(() => {}, []);
 
   return (
     <div>
